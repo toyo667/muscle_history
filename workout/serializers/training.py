77f2201b1077b40ac4ebf5bc923dca40ec1ec762
Hiro <poyo2c@gmail.com>
2024-01-01 18:56:37 +0900

    ワークアウトを入力する際に、前回のワークアウトを参照できるように修正

--- workout/serializers/training.py
+++ workout/serializers/training.py
@@ -1,4 +1,5 @@
 from dataclasses import field
+from pkg_resources import require
 from rest_framework import serializers
 
 from workout.models.training import Workout, WorkoutItem, WorkoutSession
@@ -20,3 +21,8 @@ class WorkoutSerializer(serializers.ModelSerializer):
     class Meta:
         model = Workout
         fields = ["id", "training_item", "feeling", "session", "rep_count", "set_count", "weight_kg", "trained_at"]
+
+
+class RecentWorkoutParamSerializer(serializers.Serializer):
+    training_item = serializers.UUIDField()
+    session = serializers.UUIDField(required=False)
--- workout/views/training.py
+++ workout/views/training.py
@@ -1,14 +1,19 @@
 from rest_framework import viewsets
 from workout.models.training import Workout, WorkoutItem, WorkoutSession
 
-from workout.serializers.training import WorkoutItemSerializer, WorkoutSerializer, WorkoutSessionSerializer
+from workout.serializers.training import (
+    RecentWorkoutParamSerializer,
+    WorkoutItemSerializer,
+    WorkoutSerializer,
+    WorkoutSessionSerializer,
+)
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
 from rest_framework import status
 from drf_spectacular.utils import extend_schema
 from rest_framework.response import Response
 from django_filters import rest_framework as filters
-
+from django.db.models import Q
 import django_filters.rest_framework
 
 
@@ -70,7 +75,27 @@ class WorkoutViewSet(viewsets.ModelViewSet):
     filterset_fields = ["session"]
 
     def get_queryset(self):
-        return Workout.objects.filter(user=self.request.user)
+        return Workout.objects.filter(user=self.request.user).select_related("training_item", "session")
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    @extend_schema(parameters=[RecentWorkoutParamSerializer], responses={200: WorkoutSerializer(many=True), 204: {}})
+    @extend_schema(description="指定したワークアウト種目の直近の結果を取得する。セッションIDは除外用", methods=["GET"])
+    @action(detail=False, methods=["get"])
+    def recent_workout(self, request):
+        req_serializer = RecentWorkoutParamSerializer(data=request.query_params)
+        req_serializer.is_valid(raise_exception=True)
+        training_item = req_serializer.validated_data.get("training_item")  # type: ignore
+        session = req_serializer.validated_data.get("session")  # type: ignore
+        exclude = Q(session=session) if session else Q()
+        recent_workout = self.get_queryset().order_by("-trained_at").filter(training_item=training_item).exclude(exclude).first()
+        if not recent_workout:
+            return Response({}, status.HTTP_204_NO_CONTENT)
+
+        # 同じセッションの同じ種目のワークアウトをすべて取得
+        samesession_workouts = self.get_queryset().filter(session=recent_workout.session, training_item=training_item)
+        response_datas = []
+        for workout in samesession_workouts:
+            response_datas.append(self.get_serializer(workout).data)
+        return Response(response_datas, status.HTTP_200_OK)
